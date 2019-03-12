a8b4c34429d4604691ae23cd0db5daa57854acb7
Jinz <Jinzhao_wu@hotmail.com>
2019-03-12 22:51:59 +1100

    Updated settings

--- Source/ProjectMMW/ProjectMMWGameMode.cpp
+++ Source/ProjectMMW/ProjectMMWGameMode.cpp
@@ -7,7 +7,7 @@
 AProjectMMWGameMode::AProjectMMWGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
